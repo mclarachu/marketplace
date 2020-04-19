4448c44cce3ab890a6f98213fba91fa3f7ad56ae
hanisahhilmi <hanisahhilmi@gmail.com>
2020-04-19 10:56:48 +0800

    made small changes

--- marketplace/profile/urls.py
+++ marketplace/profile/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from . import views
 
-app_name='profile'
-
 urlpatterns = [
     path('add_product',views.add_product,name='add_product'),
     path('add_address',views.add_address,name='add_address'),
