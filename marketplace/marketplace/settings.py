5cc0bf925bba891a7fe045c0d1bc57bd32355daa
claraj <clarajchu@gmail.com>
2020-04-19 14:02:22 -0400

    changed STATIC_ROOT

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = '/Users/hanisahhilmi/Desktop/staticfiles/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATIC_URL = '/static/'
 LOGIN_URL = '/account_access/login'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/Users/hanisahhilmi/Desktop/staticfiles/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
 
