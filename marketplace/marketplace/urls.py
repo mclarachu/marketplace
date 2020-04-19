27dece1f12cfd74e1a11fd12e38614308ecb88a7
hanisahhilmi <hanisahhilmi@gmail.com>
2020-04-19 11:59:00 +0800

    code to serve media files

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('store.urls')),
@@ -22,5 +24,4 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('account_access/',include('account_access.urls')),
     path('admin/', admin.site.urls),
-
-]
+]+ static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
