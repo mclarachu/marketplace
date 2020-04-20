0e5893efbfdb76e3e12461b92758d6e030520f0a
claraj <clarajchu@gmail.com>
2020-04-20 11:56:29 -0400

    testing CSS

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('account_access/',include('account_access.urls')),
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
-]+ static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
