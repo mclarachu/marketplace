07437dfa06c2709d40364b90abe7681f57ddbc70
hanisahhilmi <hanisahhilmi@gmail.com>
2020-04-21 09:50:26 +0800

    completed checking out functionality

--- marketplace/profile/admin.py
+++ marketplace/profile/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product,Basket,ShippingAddress,OrderHistory,ItemBasket
+from .models import Product,Basket,ShippingAddress,OrderHistory,ItemBasket,ItemOrder
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Basket)
 admin.site.register(ShippingAddress)
 admin.site.register(OrderHistory)
 admin.site.register(ItemBasket)
+admin.site.register(ItemOrder)
