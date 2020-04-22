f6bb31542e8caf9ca477f5879296268cc2a30b7a
claraj <clarajchu@gmail.com>
2020-04-22 12:27:33 -0400

    added cases to test_models

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # marketplace
-Artisanal marketplace project with Hanisah Hilmi
 
 Group Members:
 
-Hanisah Hilmi - 260794101
-Clara Chu - 260727618
+<li> Hanisah Hilmi (260794101) 
+<li> Clara Chu (260727618)
 
-For Channels and Redis server, we had to first install brew on MacOS to download openssl, and then was able to install channels and redis.
+For Channels and Redis server, we had to first install brew on MacOS to download openssl, and then we were able to install channels and redis for websockets.
 
 
--- marketplace/profile/tests/tests_models.py
+++ marketplace/profile/tests/tests_models.py
@@ -1,35 +1,32 @@
 from django.test import TestCase
 from django.urls import reverse
-from profile.models import Profile
+from profile.models import Profile, Basket, ShippingAddress, OrderHistory
 from django.contrib.auth.models import User
+
 
 class ModelTestCase(TestCase):
 
     def setUp(self):
         userTest = User.objects.create_user(username='test', password='test1234', email='test@mail.com')
         userTest.save()
-        userTest.profile.phone='911'
-        userTest.save()
 
     def test_profile_is_available(self):
         userTest = User.objects.get(username='test')
-        profileTest = Profile.objects.get(user=userTest, phone='911')
+        profileTest = Profile.objects.get(user=userTest)
         self.assertFalse(profileTest.is_available)
 
+    def test_basket_total_amount(self):
+        userTest = User.objects.get(username='test')
+        basketTest = Basket.objects.create(owner=userTest)
+        self.assertEquals(basketTest.totalAmount, 0)
 
+    def test_shipping_address(self):
+        userTest = User.objects.get(username='test')
+        shipAdd = ShippingAddress.objects.create(user=userTest,country='Canada',street1='121 Spring Drive',city='TO',province='ON',postal_code='M2N 7F2')
+        self.assertEquals(shipAdd.country, 'Canada')
 
-
-
-
-
-
-
-
-
-
-
-    #     class Profile(models.Model):
-    # user = models.OneToOneField(User,on_delete=models.CASCADE)
-    # phone = models.PositiveIntegerField(null=True)
-    # is_available = models.BooleanField(default=False)
-
+    def test_order_history(self):
+        userTest = User.objects.get(username='test')
+        shipAdd = ShippingAddress.objects.create(user=userTest,country='Canada',street1='121 Spring Drive',city='TO',province='ON',postal_code='M2N 7F2')
+        orderHis = OrderHistory.objects.create(user=userTest, shipped=shipAdd,total_payment='30')
+        self.assertEquals(orderHis.shipped.country, 'Canada')
